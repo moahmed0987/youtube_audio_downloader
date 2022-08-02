395948a128e24296238985147c2f63961c6bf1db
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2022-08-02 20:46:52 +0100

    now saves downloads into own directory

--- youtube_to_mp3.py
+++ youtube_to_mp3.py
@@ -1,9 +1,9 @@
 from pytube import Playlist
+import os
 
 
 link = "https://www.youtube.com/playlist?list=PL2788304DC59DBEB4"
 pl = Playlist(link)
 for video in pl.videos:
-    video.streams.get_audio_only().download()
+    video.streams.get_audio_only().download(os.getcwd() + "/audio")
     print(f"{video.title} downloaded.")
-    
